9b815dd05b773bd88d96371b2110610de0851656
Aleia82 <TLarkins@fullsail.edu>
2014-02-16 22:03:06 -0500

    Changes to Associatingjs file

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Gridaverse on 2/16/14.
+ */
+alert("test");
---
+++ Expressions_Industry/js/inventory.js
@@ -0,0 +1,7 @@
+// This is a script by Tanisha Larkins to calculate Inventory of various office supplies
+
+var pens;
+var paper;
+var staples;
+
+
