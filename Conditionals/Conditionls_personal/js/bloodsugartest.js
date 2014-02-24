e5e8e322f21ed1c0c8fae64143f23ff9060f950a
Aleia82 <TLarkins@fullsail.edu>
2014-02-23 23:47:05 -0500

    Conditionals Personal
    Code tested, and working.  If else code tested.  Program working

--- Conditionals/Conditionls_personal/js/bloodsugartest.js
+++ Conditionals/Conditionls_personal/js/bloodsugartest.js
@@ -12,14 +12,21 @@ var sugarLevelLow = "Please have something to eat";
 alert("Hello and welcome to Tanisha Larkins Blood Sugar Tester.  This Test will help you make a decision about what to do based on your blood sugar input");
 //Prompting user to enter blood sugar value
 var sugarLevel =prompt("Please enter your Sugar Level ");
-//
-var input;
-if(input < 50){
-    sugarLevelLow();
-} else
 
-if (input > 110){
-    sugarLevelHigh();
+
+//  this is the is the coding and result for the coding input
+
+if (sugarLevel<50)
+{
+    alert (sugarLevelLow);
+}
+else if (sugarLevel>110)
+{
+    alert(sugarLevelHigh);
+}
+else
+{
+    alert("You blood sugar is fine.  Continue to monitor yourself");
 }
 
 
