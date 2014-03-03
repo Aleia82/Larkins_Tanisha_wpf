bee2cd7ba9d5384e41a58103f88ba63324c0f1ca
Aleia82 <TLarkins@fullsail.edu>
2014-03-02 21:27:23 -0500

    Fingers
    prompt for finger count made and js linked to html file

---
+++ Functions/Functions_Wacky/js/fingers.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Tanisha Larkins on 2/26/14.
+ */
+var name = prompt ("Hello what is your name?");
+alert ("Hello " + name + " very nice to meet you");
+var fingers = prompt ("How many fingers am I holding up?");
+
