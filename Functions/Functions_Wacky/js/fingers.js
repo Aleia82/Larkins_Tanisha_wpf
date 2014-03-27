f43fc3e5b070736b8260154dbf43c6f4aa932711
Aleia82 <TLarkins@fullsail.edu>
2014-03-27 19:26:44 -0400

    Function_Wacky
    Finger counting script with function, else if condition, functions—loop
    to be added

--- Functions/Functions_Wacky/js/fingers.js
+++ Functions/Functions_Wacky/js/fingers.js
@@ -6,26 +6,28 @@
 var userName = prompt("Hello and welcome to Wacko the Wacky Computer Finger counting Game.  Please enter you name ");//this is the prompt for user to enter his or her name
 alert("Hello " + userName + " it is very nice to meet you.  In order to play this game you must guess how many fingers I am holding up.  You will have 10 chances to guess.");//alert giving directions on how to play game
 var fingers = prompt("Please enter the number of finger I am holding up.");
-fingers=0; //Prompt or user to enter in guess on how many fingers computer is holding up
+
 
 //function to calculate user's answer
 function fingerCount(){
-    if(fingers=0){
-        //run this if fingers=0
-        alert("Finally you realized I am a computer and have no fingers!");
-
-    }else if(fingers=10){// else if condition for fingers variable equal to 10
-        //run if fingers variable is equal to 10
-        alert("Can you Type holding up 10 fingers?  yeah I didn't think so");
-
-    }else if(fingers > 1 && fingers < 5){//else if condition with logical operator for fingers variable greater than 1 and less than 5
-        //run if finger is greater than 1 and less than 5
-        alert("hmmmmm not correct--Please try again")
-    }else if (fingers > 5 && fingers < 10){
-        alers ("")
-
+    if(fingers == 0){
+       alert("Finally you get it--I am a computer!  I don't have fingers."); //alert if variable fingers is equal to 0
+    }else if (fingers == 10){
+        alert("humans have 10 fingers--I am not a human!"); //alert if variable fingers is equal to 10
+    }else if (fingers > 10){
+        alert("What are you thinking?  You don't even have " + fingers + " fingers");//alert if  variable fingers is greater than 10
+    }else if (fingers >= 1 && fingers <= 5){
+       alert(" I am trying to think of what animal has " + fingers + " fingers....regardless I am not that animal!"); //alert if variable fingers is greater than 1 and less that 5
+    }else if (fingers >5 && fingers < 10){
+       alert( "Okay "+ userName + " that answer is not correct try again!"); //alert if variable fingers is greater than 5 and less than 10
     }
-}
 
-fingerCount();//calling fingerCount function
+}
+fingerCount(); //calling fingerCount function
+
+
+
+
+
+
 
