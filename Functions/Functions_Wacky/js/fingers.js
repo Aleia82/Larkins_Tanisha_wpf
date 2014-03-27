946ba715bc26617c4d3bb54b25b03115550ec062
Aleia82 <TLarkins@fullsail.edu>
2014-03-27 13:20:41 -0400

    Functions_Wacky
    Function added along with Conditions and Logical Operator

--- Functions/Functions_Wacky/js/fingers.js
+++ Functions/Functions_Wacky/js/fingers.js
@@ -8,5 +8,24 @@ alert("Hello " + userName + " it is very nice to meet you.  In order to play thi
 var fingers = prompt("Please enter the number of finger I am holding up.");
 fingers=0; //Prompt or user to enter in guess on how many fingers computer is holding up
 
+//function to calculate user's answer
+function fingerCount(){
+    if(fingers=0){
+        //run this if fingers=0
+        alert("Finally you realized I am a computer and have no fingers!");
 
+    }else if(fingers=10){// else if condition for fingers variable equal to 10
+        //run if fingers variable is equal to 10
+        alert("Can you Type holding up 10 fingers?  yeah I didn't think so");
+
+    }else if(fingers > 1 && fingers < 5){//else if condition with logical operator for fingers variable greater than 1 and less than 5
+        //run if finger is greater than 1 and less than 5
+        alert("hmmmmm not correct--Please try again")
+    }else if (fingers > 5 && fingers < 10){
+        alers ("")
+
+    }
+}
+
+fingerCount();//calling fingerCount function
 
