a465b756dc297dc713da6efeae8f3d8351d994c6
Aleia82 <TLarkins@fullsail.edu>
2014-03-27 12:51:40 -0400

    Functions_Wacky
    Initial gam intro and user prompt added

--- Functions/Functions_Wacky/js/fingers.js
+++ Functions/Functions_Wacky/js/fingers.js
@@ -1,4 +1,6 @@
 /**
  * Created by Tanisha Larkins on 3/26/2014
- * This is a script where the user gets 10 tries to guess the computers age
+ * Wacky script where user gets 10 chances to guess how many fingers computer is holding up
  */
+
+var userName = prompt("Hello and welcome to Wacko the Wacky Computer Finger counting Game.  Please enter you name ");//this is the prompt for user to enter his or her name
