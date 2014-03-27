4235bd55b1a8851f5474b6a77333bd9da6d5afdd
Aleia82 <TLarkins@fullsail.edu>
2014-03-27 12:58:42 -0400

    Functions_Wacky
    // alert giving directions as to how to play game

--- Functions/Functions_Wacky/js/fingers.js
+++ Functions/Functions_Wacky/js/fingers.js
@@ -4,3 +4,9 @@
  */
 
 var userName = prompt("Hello and welcome to Wacko the Wacky Computer Finger counting Game.  Please enter you name ");//this is the prompt for user to enter his or her name
+alert("Hello " + userName + " it is very nice to meet you.  In order to play this game you must guess how many fingers I am holding up.  You will have 10 chances to guess.");//alert giving directions on how to play game
+var fingers = prompt("Please enter the number of finger I am holding up.");
+fingers=0; //Prompt or user to enter in guess on how many fingers computer is holding up
+
+
+
