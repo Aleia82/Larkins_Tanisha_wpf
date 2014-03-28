a3ce69c2505f59e6d5aeecd037fe1af245f696d7
Aleia82 <TLarkins@fullsail.edu>
2014-03-27 22:39:34 -0400

    Functions_Personal
    Personal script to calculate medication dosage based on weight and
    measurement of two different medications

--- Functions/Functions_Personal/js/medication.js
+++ Functions/Functions_Personal/js/medication.js
@@ -1,5 +1,18 @@
 // Created on 3/26/2014 by Tanisha Larkins
-//This is a script to  calculate the amount if medication a child is to be giving according to the child;s age and weight
+//This is a script to  calculate the amount if medication a child is to be giving according to the child weight and dosage amount
 
 
+var calculate;
+calculate = function (medication1, medication2, childsWeight){
+    var medication1 = prompt("Please enter in the value for the 1st medication");//medication variable for medication 1 and prompt to enter in information
+    var medication2 = prompt("Please enter in the value for the ");//medication variable for medication 2 and prompt to enter in information
+    var childsWeight = prompt("Please enter child weight in lbs");//child's weight variable and prompt to enter in information
+    var totalMed = (medication1 * medication2);// total medication calculation of medication 1 and medication 2
+    var WeightAndTotalMedCalm = (totalMed * childsWeight);
+    var ccToOzConvert = (WeightAndTotalMedCalm * 240);
+    var final = (.03 * ccToOzConvert);
+    alert("Please give the child " + final + " oz of medications");
+};
+
+calculate();
 
