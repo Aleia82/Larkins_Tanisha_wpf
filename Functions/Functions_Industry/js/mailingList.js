f1f8d622548c1ae5f4e7c6b5fe2c2cd8077c7287
Aleia82 <TLarkins@fullsail.edu>
2014-03-27 23:27:43 -0400

    Function_Industry
    variables declared

--- Functions/Functions_Industry/js/mailingList.js
+++ Functions/Functions_Industry/js/mailingList.js
@@ -1,2 +1,19 @@
 //Created by Tanisha Larkins on 3/26/2014
 //This is a script to form a mailing list based on user's input
+
+function name(){
+   var firstName = prompt("Please enter your Firstname");
+    firstName = confirm("You entered in " + firstName + " .  Is this correct?")? lastName: name;
+   var lastName = prompt ("Please enter your Lastname");
+    confirm("You entered in " + lastname + " .  Is this correct?")? lastName:;
+}
+name();
+
+
+function address(){
+
+}
+
+function phoneNumber(){
+
+}
